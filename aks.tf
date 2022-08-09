a62b43fdc885fa6223a4c895e3e4e2a07abf505f
Andrey Olishchuk <bobroe.web@gmail.com>
2022-08-09 23:51:11 +0200

    renamed

--- aks.tf
+++ aks.tf
@@ -1,5 +1,5 @@
 resource "azurerm_kubernetes_cluster" "kubernetes" {
-  name                = "kubernetes"
+  name                = "salento"
   location            = azurerm_resource_group.default.location
   resource_group_name = azurerm_resource_group.default.name
   dns_prefix          = "kubernetes"
